60ba68a3db9b1b17166972bf69be8b8f007abd6c
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-06-14 07:20:06 +0100

    all working

--- src/grb.jl
+++ src/grb.jl
@@ -1,4 +1,13 @@
 module grb
+using Gurobi
+
+const GRB_ENV = Ref{Gurobi.Env}()
+
+function __init__()
+    GRB_ENV[] = Gurobi.Env()
+    return
+end
+
 include("o.jl")
 
 export set_solver
--- src/o.jl
+++ src/o.jl
@@ -1,8 +1,6 @@
 using Gurobi, JuMP
-const GRB_ENV = Gurobi.Env()
-@show GRB_ENV
 
 function set_solver(model::Model)
     set_silent(model)
-    set_optimizer(model, () -> Gurobi.Optimizer(GRB_ENV))
+    set_optimizer(model, () -> Gurobi.Optimizer(GRB_ENV[]))
 end
